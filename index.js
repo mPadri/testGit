7abb030620442d7b2cdf3f0549cd30a3c6c7fa6c
mPadri <cvadhri@gmail.com>
2020-01-07 14:28:42 +0700

    push ke 3

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req,res)=>{
-    return res.send(`welcome`);
-})
+// app.get('/', (req,res)=>{
+//     return res.send(`welcome`);
+// })
 
 app.post('/', (req,res)=>{
     let email = req.body.email
     return res.send(`email value ${email}`)
+})
+
+app.get('/', (req,res)=>{
+    var x, y, z
+    x = 5
+    y = 3
+    z = x + y
+
+    return res.send(`value z is ${z}`)
+})
+
+app.post('/tambah', (req,res)=>{
+    var a, b, c
+
+    a = parseInt(req.body.a)
+    b = parseInt(req.body.b)
+    c = a + b
+
+    console.log(typeof a)
+    console.log(typeof b)
+    console.log(typeof c)
+
+    return res.send(`result value ${c}`)
 })
 
 app.listen(3000, () => {
